7bc6bfb112533c1afa0a6a141b002e933d69bfd9
Caio Gonçalves <caio.goncalves@cepedi.org.br>
2023-10-30 15:27:36 -0300

    corrigindo a hierarquia das classes

--- part1/index.cpp
+++ part1/index.cpp
@@ -4,25 +4,29 @@
 
 using namespace std;
 
-class Data {
+class Data
+{
 	int dia, mes, ano;
-	public:
-	
+
+public:
 	/*
-	O método abaixo retornará -1 se d1 é anterior a d2
-	Retornará 0 se d1 = d2
-	Retornará +1 se d1 é posterior a d2
-	*/	
-	static int compara(Data d1, Data d2) { 
+	O mï¿½todo abaixo retornarï¿½ -1 se d1 ï¿½ anterior a d2
+	Retornarï¿½ 0 se d1 = d2
+	Retornarï¿½ +1 se d1 ï¿½ posterior a d2
+	*/
+	static int compara(Data d1, Data d2)
+	{
 		return 0;
 	}
-	
-	Data (int _dia, int _mes, int _ano) {
+
+	Data(int _dia, int _mes, int _ano)
+	{
 		dia = _dia;
 		mes = _mes;
 		ano = _ano;
 	}
-	string toString() {
+	string toString()
+	{
 		string ret = "";
 		ret.append(to_string(dia));
 		ret.append("/");
@@ -33,144 +37,156 @@ class Data {
 	}
 };
 
-class Lista {
-	public:
-	virtual void entradaDeDados() =0;
-	virtual void mostraMediana() =0;
-	virtual void mostraMenor() =0;
-	virtual void mostraMaior() =0;
+class Lista
+{
+public:
+	virtual void entradaDeDados() = 0;
+	virtual void mostraMediana() = 0;
+	virtual void mostraMenor() = 0;
+	virtual void mostraMaior() = 0;
 };
 
-class ListaNomes {
+class ListaNomes : public Lista
+{
 	vector<string> lista;
-	
-	public:
-	
+
+public:
 	/*
-	O método abaixo pergunta ao usuários quantos
-	elementos vão existir na lista e depois
-	solicita a digitação de cada um deles
-	*/	
-	void entradaDeDados() {
+	O mï¿½todo abaixo pergunta ao usuï¿½rios quantos
+	elementos vï¿½o existir na lista e depois
+	solicita a digitaï¿½ï¿½o de cada um deles
+	*/
+	void entradaDeDados()
+	{
 		lista.push_back("Teste");
 	}
-		
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de strings" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar o primeiro nome alfabeticamente" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar o ultimo nome alfabeticamente" << endl;
 	}
 };
 
-class ListaDatas  {
+class ListaDatas : public Lista
+{
 	vector<Data> lista;
-	
-	public:
-		
+
+public:
 	/*
-	O método abaixo pergunta ao usuários quantos
-	elementos vão existir na lista e depois
-	solicita a digitação de cada um deles
-	*/	
-	void entradaDeDados() {
-		
+	O mï¿½todo abaixo pergunta ao usuï¿½rios quantos
+	elementos vï¿½o existir na lista e depois
+	solicita a digitaï¿½ï¿½o de cada um deles
+	*/
+	void entradaDeDados()
+	{
 	}
-	
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de datas" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar a primeira data cronologicamente" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar a ultima data cronologicamente" << endl;
 	}
 };
 
-class ListaSalarios  {
+class ListaSalarios : public Lista
+{
 	vector<float> lista;
-	
-	public:
-	
+
+public:
 	/*
-	O método abaixo pergunta ao usuários quantos
-	elementos vão existir na lista e depois
-	solicita a digitação de cada um deles
-	*/	
-	void entradaDeDados() {
-		
+	O mï¿½todo abaixo pergunta ao usuï¿½rios quantos
+	elementos vï¿½o existir na lista e depois
+	solicita a digitaï¿½ï¿½o de cada um deles
+	*/
+	void entradaDeDados()
+	{
 	}
-			
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de salarios" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar o menor dos salarios" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar o maior dos salarios" << endl;
 	}
 };
 
-
-class ListaIdades  {
+class ListaIdades : public Lista
+{
 	vector<int> lista;
-	
-	public:
-		
-		/*
-	O método abaixo pergunta ao usuários quantos
-	elementos vão existir na lista e depois
-	solicita a digitação de cada um deles
-	*/	
-	void entradaDeDados() {
-		
+
+public:
+	/*
+O mï¿½todo abaixo pergunta ao usuï¿½rios quantos
+elementos vï¿½o existir na lista e depois
+solicita a digitaï¿½ï¿½o de cada um deles
+*/
+	void entradaDeDados()
+	{
 	}
-	
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de idades" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar a menor das idades" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar a maior das idades" << endl;
 	}
 };
- 
-int main () {
-	vector<Lista*> listaDeListas;
-	
+
+int main()
+{
+	vector<Lista *> listaDeListas;
+
 	ListaNomes listaNomes;
 	listaNomes.entradaDeDados();
 	listaDeListas.push_back(&listaNomes);
-	
+
 	ListaDatas listaDatas;
 	listaDatas.entradaDeDados();
 	listaDeListas.push_back(&listaDatas);
-	
+
 	ListaSalarios listaSalarios;
 	listaSalarios.entradaDeDados();
 	listaDeListas.push_back(&listaSalarios);
-	
+
 	ListaIdades listaIdades;
 	listaIdades.entradaDeDados();
 	listaDeListas.push_back(&listaIdades);
-	
-	for (Lista* l : listaDeListas) {
+
+	for (Lista *l : listaDeListas)
+	{
 		l->mostraMediana();
 		l->mostraMenor();
 		l->mostraMaior();
 	}
-	
 }
-    
-
